15a24e703799403074fad176530685536d46484d
kai20060719 <kai20060719@gmail.com>
2025-05-30 11:47:42 +0900

    backend에서 frontend로 정보 가져오기

---
+++ frontend/src/pages/chatpage.js
@@ -0,0 +1,23 @@
+import React, { useEffect } from 'react'
+import axios from 'axios'
+
+const [chats, setchats] = useState([])
+
+const Chatpage = () => {
+    const getchats = async () => {
+        const data = await axios.get('/api/chat');
+        setchats(data);
+    }
+ 
+    useEffect(() =>{
+        getchats();
+    }, [])
+  
+    return (
+    <div>{chats.map((chat) => (
+        <div key={chat._id}>{chat.chatName}</div>
+    ))}</div>
+  )
+}
+
+export default chatpage
